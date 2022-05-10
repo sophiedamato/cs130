698b5300ea71fa5a0132a5f8ae1ce2813da4e08b
sophiedamato <sophied'amato2023@u.northwestern.edu>
2022-05-09 22:03:16 -0500

    tutorial 6 addition

---
+++ homework/hw03/js/index.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * -------------------------------------
+ * DOM Manipulation / Traversal Activity
+ * -------------------------------------
+ * 
+ * 1. Create and attach an event handler (function) to each ".image" 
+ * element so that when the ".image" element is clicked, the corresponding 
+ * image loads in the .featured image element.
+ * 
+ * 2. Create event handlers for the next and previous buttons. The next button should
+ *    show the next image in the thumbnail list. The previous should show the previous.
+ * 
+ * 3. If you get to the end, start at the beginning. 
+ * 
+ * 4. If you get to the beginning, loop around to the end.
+ * 
+ * 
+ */
+
+const images = [
+    'images/field1.jpg',
+    'images/purple.jpg',
+    'images/jar.jpg',
+    'images/green.jpg',
+    'images/green1.jpg',
+    'images/purple1.jpg',
+    'images/magnolias.jpg',
+    'images/daisy1.jpg'
+];
+
+const initScreen = () => {
+    images.forEach((image, idx) => {
+        document.querySelector('.cards').innerHTML += `
+        <li class="card">
+            <button class="image" 
+            onclick="handleThumbnailClick(event)"
+                style="background-image:url('${image}')"
+                data-index=${idx}"
+                aria-label="Displays image ${idx} in the main panel."></button>
+        </li>`;
+    });
+};
+
+const handleThumbnailClick = ev => {
+    console.log(ev);
+   const elem = ev.currentTarget;
+   console.log(elem)
+   const bgImage = elem.style.backgroundImage;
+   document.querySelector('.featured_image').style.backgroundImage = bgImage;
+
+
+}
+
+initScreen();
